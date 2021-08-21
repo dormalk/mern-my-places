63f7d7ed337d745c2f68da5c5ce13b32ad6ad9e3
dormalka2019 <dormalk@gmail.com>
2021-08-21 12:53:25 +0300

    init side drawer

--- src/shared/components/Navigation/SideDrawer/SideDrawer.tsx
+++ src/shared/components/Navigation/SideDrawer/SideDrawer.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
+import './SideDrawer.css';
 
+class SideDrawerProps {
+    children?: ReactElement<any,any> | string;
+}
 
-const SideDrawer = () => {
-
+const SideDrawer = (props:SideDrawerProps) => {
+    return <aside className="side-drawer">
+        {props.children}
+    </aside>
 }
 
 export default SideDrawer;
